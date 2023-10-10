6e7767c02e4a23f1f361739429129fedb4da138a
Gabriel <francogm77@hotmail.com>
2023-10-09 22:51:15 -0300

    fix middleware use

--- routers/router.go
+++ routers/router.go
@@ -17,7 +17,7 @@ func configureCors() {
 	engine.Use(
 		cors.New(cors.Config{
 			AllowOrigins:     []string{"http://localhost:8000"},
-			AllowMethods:     []string{"GET", "POST"},
+			AllowMethods:     []string{"GET", "POST", "DELETE", "PUT"},
 			ExposeHeaders:    []string{"Content-Length"},
 			AllowCredentials: true,
 			MaxAge:           24 * time.Hour,
@@ -25,26 +25,24 @@ func configureCors() {
 	)
 }
 
-func configureMiddlewares() {
-
-}
-
 func ConfigureRouters(userHandler *users.Handler, expenseHandler *expenses.Handler) {
 	engine = gin.Default()
 
 	configureCors()
-	configureMiddlewares()
 
 	authGroup := engine.Group("/auth")
+
 	authGroup.POST("/signup", userHandler.Register)
 	authGroup.POST("/login", userHandler.Login)
 
 	expensesGroup := engine.Group("/expenses")
-	expensesGroup.POST("", userHandler.Authenticate, expenseHandler.CreateExpense)
-	expensesGroup.GET("", userHandler.Authenticate, expenseHandler.ListExpenses)
-	expensesGroup.GET("/:id", userHandler.Authenticate, expenseHandler.GetExpense)
-	expensesGroup.PUT("/:id", userHandler.Authenticate, expenseHandler.UpdateExpense)
-	expensesGroup.DELETE("/:id", userHandler.Authenticate, expenseHandler.DeleteExpense)
+
+	expensesGroup.Use(userHandler.Authenticate)
+	expensesGroup.POST("", expenseHandler.CreateExpense)
+	expensesGroup.GET("", expenseHandler.ListExpenses)
+	expensesGroup.GET("/:id", expenseHandler.GetExpense)
+	expensesGroup.PUT("/:id", expenseHandler.UpdateExpense)
+	expensesGroup.DELETE("/:id", expenseHandler.DeleteExpense)
 }
 
 func Start() error {
