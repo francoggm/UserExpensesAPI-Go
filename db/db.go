3a3e307c2104dadd7c4a011e2bef3bee3f817df9
Gabriel <francogm77@hotmail.com>
2023-10-14 13:43:42 -0300

    adjust compose and create tables

--- db/db.go
+++ db/db.go
@@ -8,6 +8,33 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func createTables(db *sql.DB) error {
+	_, err := db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		email VARCHAR(255) NOT NULL,
+		name VARCHAR(255) NOT NULL,
+		password BYTEA,
+		created_at TIMESTAMP NOT NULL,
+		last_login TIMESTAMP
+	);
+
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		user_id INT NOT NULL,
+		title VARCHAR(150) NOT NULL,
+		description TEXT,
+		value NUMERIC(12, 2) NOT NULL,
+		category_type SMALLINT,
+		movimentation_type SMALLINT,
+		created_at TIMESTAMP NOT NULL,
+		CONSTRAINT fk_users FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`)
+
+	return err
+}
+
 func NewDatabase() (*sql.DB, error) {
 	cfg := configs.GetConfigs()
 
@@ -16,6 +43,10 @@ func NewDatabase() (*sql.DB, error) {
 	db, err := sql.Open("postgres", sc)
 	if err != nil {
 		return nil, err
+	}
+
+	err = createTables(db); if err != nil {
+		fmt.Print("error creating tables!")
 	}
 
 	return db, nil
